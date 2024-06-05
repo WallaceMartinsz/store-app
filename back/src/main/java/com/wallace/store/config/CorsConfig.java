b1b1c6a5c47debdfb0ed9784c1b4701dfbcb2a89
Wallace Martins <martinsswallace@gmail.com>
2024-06-04 23:47:31 -0300

    chore: ajuste cors para aceitar método put

--- back/src/main/java/com/wallace/store/config/CorsConfig.java
+++ back/src/main/java/com/wallace/store/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
         public void addCorsMappings(CorsRegistry registry) {
             registry.addMapping("/**")
                     .allowedOrigins("http://localhost:3000")
-                    .allowedMethods("GET", "POST", "DELETE")
+                    .allowedMethods("GET", "POST", "DELETE", "PUT")
                     .allowedHeaders("*");
         }
 
